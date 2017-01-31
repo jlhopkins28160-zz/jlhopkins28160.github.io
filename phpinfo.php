bac9c81221a96ac2291e1b3273ffb38e8fe1682b
Johnny Hopkins <jhopkins1009@student.isothermal.edu>
2017-01-31 09:14:38 -0500

    Add files via upload

---
+++ phpinfo.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>My PHPInfo page on Github Pages</title>
+    <script>
+        var x = new Date();
+        document.write("This was uploaded to Github on ", x);
+</head>
+
+<body>
+
+    <?php
+        phpinfo();
+    <?
+
+</body>
+
+</html>
